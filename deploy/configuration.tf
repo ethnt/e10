c7102c441457b6054df3b774a1c924abf8cd7287
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2023-08-02 00:09:41 -0400

    Format Terraform files

--- deploy/configuration.tf
+++ deploy/configuration.tf
@@ -1,13 +1,13 @@
 provider "tailscale" {
-  oauth_client_id = data.sops_file.secrets.data["TAILSCALE_OAUTH_CLIENT_ID"]
+  oauth_client_id     = data.sops_file.secrets.data["TAILSCALE_OAUTH_CLIENT_ID"]
   oauth_client_secret = data.sops_file.secrets.data["TAILSCALE_OAUTH_CLIENT_SECRET"]
-  tailnet = data.sops_file.secrets.data["TAILSCALE_TAILNET"]
+  tailnet             = data.sops_file.secrets.data["TAILSCALE_TAILNET"]
 }
 
 provider "aws" {
   access_key = data.sops_file.secrets.data["AWS_ACCESS_KEY_ID"]
   secret_key = data.sops_file.secrets.data["AWS_SECRET_ACCESS_KEY"]
-  region = "us-east-2"
+  region     = "us-east-2"
 
   default_tags {
     tags = {
