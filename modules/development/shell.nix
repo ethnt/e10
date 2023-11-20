5d2b7171af0e90eb5965068b57416339f46ac3e9
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2023-11-20 11:24:06 -0500

    Set correct age key directory

--- modules/development/shell.nix
+++ modules/development/shell.nix
@@ -4,12 +4,17 @@
   perSystem = { inputs', pkgs, ... }: {
     devenv.shells.default = _:
       {
+        # TODO: Move this to be within the `keys/` directory?
+        env.SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/keys.txt";
+
         enterShell = let
           sops = pkgs.lib.getExe' pkgs.sops "sops";
           setSopsValueToEnvironmentVariable = key: ''
             export ${key}=$(${sops} -d --extract '["${key}"]' ./secrets.json)
           '';
         in ''
+          export SOPS_AGE_KEY_FILE="/Users/$USER/.config/sops/age/keys.txt"
+
           ${setSopsValueToEnvironmentVariable "AWS_ACCESS_KEY_ID"}
           ${setSopsValueToEnvironmentVariable "AWS_SECRET_ACCESS_KEY"}
         '';
