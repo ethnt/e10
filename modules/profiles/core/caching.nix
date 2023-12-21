c8bc9db072c6c1096157bb96b59bca202583572d
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2023-12-21 13:41:27 -0500

    Enable `nixConfig`, add `cachix` to all host machines (#17)

--- modules/profiles/core/caching.nix
+++ modules/profiles/core/caching.nix
@@ -1,5 +1,8 @@
-{ inputs, ... }: {
-  environment.systemPackages = [ inputs.attic.packages.x86_64-linux.attic ];
+{ inputs, pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    inputs.attic.packages.x86_64-linux.attic
+    cachix
+  ];
 
   nix.settings = {
     substituters = [
@@ -7,6 +10,7 @@
       "https://numtide.cachix.org"
       "https://attic.e10.camp"
     ];
+
     trusted-public-keys = [
       "e10.cachix.org-1:/++Tmo/ghEqnLwsQJdXn04c262agRCK5PaPYz8NcVfo="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
