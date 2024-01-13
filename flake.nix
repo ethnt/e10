7a997ad2df9bb028ddfa5a6af535f891ad45646e
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-01-13 17:16:27 -0500

    Generate GitHub Actions with Nix (#26)
    * Try Nix-based CI config
    
    * Fix typo
    
    * Use Determinate installer
    
    * Format.
    
    * Don't be so chatty
    
    * Generate with Nixago
    
    * Add `generate-ci` to `Justfile`

--- flake.nix
+++ flake.nix
@@ -6,14 +6,12 @@
       "https://nix-community.cachix.org"
       "https://e10.cachix.org"
       "https://numtide.cachix.org"
-      "https://cache.builder.e10.camp"
     ];
 
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "e10.cachix.org-1:/++Tmo/ghEqnLwsQJdXn04c262agRCK5PaPYz8NcVfo="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "cache.builder.e10.camp:lvUGwBcrxIyO5+KGymLJTqEWKJZ0wuaXPNexPENrQwA="
     ];
   };
 
@@ -45,6 +43,9 @@
 
     treefmt.url = "github:numtide/treefmt-nix";
     treefmt.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixago.url = "github:nix-community/nixago";
+    nixago.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ flake-parts, ... }:
@@ -53,7 +54,7 @@
         ./lib
 
         ./modules/development/shell.nix
-        ./modules/development/dhall.nix
+        ./modules/development/ci.nix
         ./modules/development/treefmt.nix
 
         ./modules/deploy/shell.nix
