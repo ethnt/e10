5f53bdab470c4eace358753464a9c9a70f98b464
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-01-14 16:58:05 -0500

    Add build job to CI

--- modules/deploy/shell.nix
+++ modules/deploy/shell.nix
@@ -1,7 +1,7 @@
 { self, inputs, ... }:
 let l = inputs.nixpkgs.lib // builtins;
 in {
-  perSystem = { config, pkgs, inputs', ... }: {
+  perSystem = { config, pkgs, inputs', ... }: rec {
     devenv.shells.default = let
       sshConfig = let
         hostConfigurations = l.concatStringsSep "\n" (l.attrValues (l.mapAttrs
@@ -17,8 +17,10 @@ in {
           IdentityFile keys/id_rsa
       '';
 
+      ssh = pkgs.lib.getExe' pkgs.openssh "ssh";
+
       e10-ssh = pkgs.writeShellScriptBin "e10-ssh" ''
-        ${pkgs.lib.getExe' pkgs.openssh "ssh"} -F ${sshConfig} $@
+        ${ssh} -F ${sshConfig} $@
       '';
 
       e10-mosh = pkgs.writeShellScriptBin "e10-mosh" ''
@@ -28,6 +30,10 @@ in {
       e10-scp = pkgs.writeShellScriptBin "e10-scp" ''
         ${pkgs.lib.getExe' pkgs.openssh "scp"} -F ${sshConfig} $@
       '';
+
+      e10-rsync = pkgs.writeShellScriptBin "e10-rsync" ''
+        ${pkgs.lib.getExe' pkgs.rsync "rsync"} -e "${ssh} -F ${sshConfig}" $@
+      '';
     in _: {
       env.SSH_CONFIG_FILE = sshConfig;
       packages = with inputs'; [
@@ -36,6 +42,7 @@ in {
         e10-ssh
         e10-mosh
         e10-scp
+        e10-rsync
       ];
     };
   };
--- modules/development/ci.nix
+++ modules/development/ci.nix
@@ -1,39 +1,51 @@
-{ inputs, ... }: {
+{ self, inputs, ... }:
+let l = inputs.nixpkgs.lib // builtins;
+in {
   perSystem = { pkgs, system, ... }:
     let
+      setup = [
+        {
+          name = "Checkout code";
+          uses = "actions/checkout@v3";
+        }
+        {
+          name = "Install Nix";
+          uses = "DeterminateSystems/nix-installer-action@main";
+          "with" = { extra-conf = "allow-import-from-derivation = true"; };
+        }
+        {
+          name = "Use Cachix store";
+          uses = "cachix/cachix-action@v12";
+          "with" = {
+            authToken = "\${{ secrets.CACHIX_AUTH_TOKEN }}";
+            extraPullNames = "e10,nix-community";
+            name = "e10";
+          };
+        }
+      ];
+
       workflow = {
         name = "CI";
         on.push = { };
         jobs = {
           check = {
+            name = "Check flake";
             "runs-on" = "ubuntu-latest";
-            steps = [
-              {
-                name = "Checkout code";
-                uses = "actions/checkout@v3";
-              }
-              {
-                name = "Install Nix";
-                uses = "DeterminateSystems/nix-installer-action@main";
-                "with" = {
-                  extra-conf = "allow-import-from-derivation = true";
-                };
-              }
-              {
-                name = "Use Cachix store";
-                uses = "cachix/cachix-action@v12";
-                "with" = {
-                  authToken = "\${{ secrets.CACHIX_AUTH_TOKEN }}";
-                  extraPullNames = "e10,nix-community";
-                  name = "e10";
-                };
-              }
-              {
-                run = ''
-                  nix flake check --impure --all-systems --accept-flake-config --show-trace
-                '';
-              }
-            ];
+            steps = setup ++ [{
+              run = ''
+                nix flake check --impure --all-systems --accept-flake-config --show-trace
+              '';
+            }];
+          };
+          buildSystem = {
+            name = "Build system";
+            "runs-on" = "ubuntu-latest";
+            strategy.matrix.host = l.attrNames self.nixosConfigurations;
+            steps = setup ++ [{
+              run = ''
+                nix build .#nixosConfigurations.''${{ matrix.host }}.config.system.build.toplevel --accept-flake-config --show-trace
+              '';
+            }];
           };
         };
       };
