34aa2075e45e65c4c24c8063ea127e980543fa16
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-01-14 18:45:37 -0500

    Build on GitHub Actions (#29)
    * Add build job to CI
    
    * Only build on pull request
    
    * Build on push to `main` as well
    
    * Only build on push to `main`

--- modules/development/ci.nix
+++ modules/development/ci.nix
@@ -1,52 +1,79 @@
-{ inputs, ... }: {
+{ self, inputs, ... }:
+let l = inputs.nixpkgs.lib // builtins;
+in {
   perSystem = { pkgs, system, ... }:
     let
-      workflow = {
-        name = "CI";
+      setup = [
+        {
+          name = "Checkout code";
+          uses = "actions/checkout@v3";
+        }
+        {
+          name = "Install Nix";
+          uses = "DeterminateSystems/nix-installer-action@main";
+          "with" = { extra-conf = "allow-import-from-derivation = true"; };
+        }
+        {
+          name = "Use Cachix store";
+          uses = "cachix/cachix-action@v12";
+          "with" = {
+            authToken = "\${{ secrets.CACHIX_AUTH_TOKEN }}";
+            extraPullNames = "e10,nix-community";
+            name = "e10";
+          };
+        }
+      ];
+
+      checkWorkflow = {
+        name = "Check";
         on.push = { };
         jobs = {
           check = {
+            name = "Check flake";
             "runs-on" = "ubuntu-latest";
-            steps = [
-              {
-                name = "Checkout code";
-                uses = "actions/checkout@v3";
-              }
-              {
-                name = "Install Nix";
-                uses = "DeterminateSystems/nix-installer-action@main";
-                "with" = {
-                  extra-conf = "allow-import-from-derivation = true";
-                };
-              }
-              {
-                name = "Use Cachix store";
-                uses = "cachix/cachix-action@v12";
-                "with" = {
-                  authToken = "\${{ secrets.CACHIX_AUTH_TOKEN }}";
-                  extraPullNames = "e10,nix-community";
-                  name = "e10";
-                };
-              }
-              {
-                run = ''
-                  nix flake check --impure --all-systems --accept-flake-config --show-trace
-                '';
-              }
-            ];
+            steps = setup ++ [{
+              run = ''
+                nix flake check --impure --all-systems --accept-flake-config --show-trace
+              '';
+            }];
           };
         };
       };
-      ci = inputs.nixago.lib.${system}.make {
-        data = workflow;
-        output = ".github/workflows/ci.yml";
+
+      buildWorkflow = {
+        name = "Build";
+        on.push.branches = [ "main" ];
+        jobs = {
+          buildSystem = {
+            name = "Build system";
+            "runs-on" = "ubuntu-latest";
+            strategy.matrix.host = l.attrNames self.nixosConfigurations;
+            steps = setup ++ [{
+              run = ''
+                nix build .#nixosConfigurations.''${{ matrix.host }}.config.system.build.toplevel --accept-flake-config --show-trace
+              '';
+            }];
+          };
+        };
+      };
+
+      check = inputs.nixago.lib.${system}.make {
+        data = checkWorkflow;
+        output = ".github/workflows/check.yml";
+        format = "yaml";
+        hook.mode = "copy";
+      };
+      build = inputs.nixago.lib.${system}.make {
+        data = buildWorkflow;
+        output = ".github/workflows/build.yml";
         format = "yaml";
         hook.mode = "copy";
       };
     in {
       apps = {
         generate-ci.program = pkgs.writeShellScriptBin "generate-ci" ''
-          ${ci.shellHook}
+          ${check.shellHook}
+          ${build.shellHook}
         '';
       };
     };
