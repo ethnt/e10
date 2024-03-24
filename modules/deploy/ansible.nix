67ce7f300d45f721e1caa1687b0daa2056caaab7
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-03-24 19:10:18 -0400

    Use `mitogen` for Ansible

--- modules/deploy/ansible.nix
+++ modules/deploy/ansible.nix
@@ -1,9 +1,15 @@
 {
   perSystem = { pkgs, ... }: {
     devenv.shells.default = {
-      packages = with pkgs; [ ansible ansible-lint ];
-      enterShell = ''
-        export ANSIBLE_ROLES_PATH="$DEVENV_ROOT/deploy/ansible/roles"
+      packages = with pkgs; [ ansible_2_13 ansible-lint ];
+      enterShell = let
+        ansibleCfg = pkgs.writeText "ansible.cfg" ''
+          [defaults]
+          strategy_plugins = ${pkgs.python311Packages.mitogen}/lib/python3.11/site-packages/ansible_mitogen/plugins/strategy
+          strategy = mitogen_linear
+        '';
+      in ''
+        export ANSIBLE_CONFIG=${ansibleCfg}
       '';
     };
   };
