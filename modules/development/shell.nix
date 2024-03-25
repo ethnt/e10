cb9101cc1c5ae08dba785572b9a8521ade758d2d
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-03-25 18:34:44 -0400

    Guess that bug with checking containers is still there

--- modules/development/shell.nix
+++ modules/development/shell.nix
@@ -2,35 +2,38 @@
   imports = [ inputs.devenv.flakeModule ];
 
   perSystem = { pkgs, system, ... }: {
-    devenv.shells.default = _: {
-      # TODO: Move this to be within the `keys/` directory?
-      env.SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/keys.txt";
+    devenv.shells.default = _:
+      {
+        # TODO: Move this to be within the `keys/` directory?
+        env.SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/keys.txt";
 
-      enterShell = let
-        sops = pkgs.lib.getExe' pkgs.sops "sops";
-        setSopsValueToEnvironmentVariable = key: secretsFile: ''
-          export ${key}=$(${sops} -d --extract '["${key}"]' ${secretsFile})
+        enterShell = let
+          sops = pkgs.lib.getExe' pkgs.sops "sops";
+          setSopsValueToEnvironmentVariable = key: secretsFile: ''
+            export ${key}=$(${sops} -d --extract '["${key}"]' ${secretsFile})
+          '';
+        in ''
+          export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
+
+          ${setSopsValueToEnvironmentVariable "AWS_ACCESS_KEY_ID"
+          "./secrets.json"}
+          ${setSopsValueToEnvironmentVariable "AWS_SECRET_ACCESS_KEY"
+          "./secrets.json"}
+
+          export TAILSCALE_AUTH_KEY=$(${sops} -d --extract '["tailscale_auth_key"]' ./modules/profiles/networking/tailscale/secrets.json)
         '';
-      in ''
-        export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
 
-        ${setSopsValueToEnvironmentVariable "AWS_ACCESS_KEY_ID"
-        "./secrets.json"}
-        ${setSopsValueToEnvironmentVariable "AWS_SECRET_ACCESS_KEY"
-        "./secrets.json"}
-
-        export TAILSCALE_AUTH_KEY=$(${sops} -d --extract '["tailscale_auth_key"]' ./modules/profiles/networking/tailscale/secrets.json)
-      '';
-
-      packages = with pkgs; [
-        inputs.devenv.packages.${system}.default
-        cachix
-        deadnix
-        just
-        statix
-        sops
-        nix-output-monitor
-      ];
-    };
+        packages = with pkgs; [
+          inputs.devenv.packages.${system}.default
+          cachix
+          deadnix
+          just
+          statix
+          sops
+          nix-output-monitor
+        ];
+      } // {
+        containers = { };
+      };
   };
 }
