e44f6e8bfc3c0252ccec828d80ea028b3e7d63e6
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-03-25 18:27:00 -0400

    Upgrade to `devenv` `1.0.0`

--- modules/development/shell.nix
+++ modules/development/shell.nix
@@ -1,31 +1,36 @@
 { inputs, ... }: {
   imports = [ inputs.devenv.flakeModule ];
 
-  perSystem = { pkgs, ... }: {
-    devenv.shells.default = _:
-      {
-        # TODO: Move this to be within the `keys/` directory?
-        env.SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/keys.txt";
+  perSystem = { pkgs, system, ... }: {
+    devenv.shells.default = _: {
+      # TODO: Move this to be within the `keys/` directory?
+      env.SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/keys.txt";
 
-        enterShell = let
-          sops = pkgs.lib.getExe' pkgs.sops "sops";
-          setSopsValueToEnvironmentVariable = key: secretsFile: ''
-            export ${key}=$(${sops} -d --extract '["${key}"]' ${secretsFile})
-          '';
-        in ''
-          export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
-
-          ${setSopsValueToEnvironmentVariable "AWS_ACCESS_KEY_ID"
-          "./secrets.json"}
-          ${setSopsValueToEnvironmentVariable "AWS_SECRET_ACCESS_KEY"
-          "./secrets.json"}
-
-          export TAILSCALE_AUTH_KEY=$(${sops} -d --extract '["tailscale_auth_key"]' ./modules/profiles/networking/tailscale/secrets.json)
+      enterShell = let
+        sops = pkgs.lib.getExe' pkgs.sops "sops";
+        setSopsValueToEnvironmentVariable = key: secretsFile: ''
+          export ${key}=$(${sops} -d --extract '["${key}"]' ${secretsFile})
         '';
+      in ''
+        export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
 
-        packages = with pkgs; [ cachix deadnix just statix sops ];
-      } // {
-        containers = pkgs.lib.mkForce { };
-      };
+        ${setSopsValueToEnvironmentVariable "AWS_ACCESS_KEY_ID"
+        "./secrets.json"}
+        ${setSopsValueToEnvironmentVariable "AWS_SECRET_ACCESS_KEY"
+        "./secrets.json"}
+
+        export TAILSCALE_AUTH_KEY=$(${sops} -d --extract '["tailscale_auth_key"]' ./modules/profiles/networking/tailscale/secrets.json)
+      '';
+
+      packages = with pkgs; [
+        inputs.devenv.packages.${system}.default
+        cachix
+        deadnix
+        just
+        statix
+        sops
+        nix-output-monitor
+      ];
+    };
   };
 }
