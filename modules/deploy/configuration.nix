cee6a4e40365f009ded5e2e776f4a69c7126732c
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-03-28 08:32:17 -0400

    Clean up Colmena output generation

--- modules/deploy/configuration.nix
+++ modules/deploy/configuration.nix
@@ -6,18 +6,23 @@ let
   deployableConfigurations =
     filterAttrs (_: c: c.config.deployment.deployable) self.nixosConfigurations;
 
-  mkColmenaOutput = configurations:
-    ({
-      meta = {
-        nixpkgs = withSystem "x86_64-linux" (ctx: ctx.pkgs);
-        nodeNixpkgs =
-          mapAttrs (_: configuration: configuration.pkgs) configurations;
-        nodeSpecialArgs =
-          mapAttrs (_: configuration: configuration._module.specialArgs)
-          configurations;
-      };
-    } // (mapAttrs
+  mkColmenaMeta = configurations: {
+    meta = {
+      nixpkgs = withSystem "x86_64-linux" (ctx: ctx.pkgs);
+      nodeNixpkgs =
+        mapAttrs (_: configuration: configuration.pkgs) configurations;
+      nodeSpecialArgs =
+        mapAttrs (_: configuration: configuration._module.specialArgs)
+        configurations;
+    };
+  };
+
+  mkColmenaNodes = configurations:
+    (mapAttrs
       (_name: configuration: { imports = configuration._module.args.modules; })
-      configurations));
+      configurations);
+
+  mkColmenaOutput = configurations:
+    (mkColmenaMeta configurations // mkColmenaNodes configurations);
 
 in { flake.colmena = mkColmenaOutput deployableConfigurations; }
