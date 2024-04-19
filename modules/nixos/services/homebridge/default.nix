7690c87c51dd2fb19abe02ab8b235899a0ebff77
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-04-18 20:14:04 -0400

    Add homebridge

---
+++ modules/nixos/services/homebridge/default.nix
@@ -0,0 +1,52 @@
+{ config, lib, ... }:
+
+with lib;
+
+let cfg = config.services.homebridge;
+in {
+  options.services.homebridge = {
+    enable = mkEnableOption "Enable Homebridge";
+
+    port = mkOption {
+      type = types.port;
+      default = 8581;
+    };
+
+    stateDir = mkOption {
+      type = types.path;
+      default = "/var/lib/homebridge";
+    };
+
+    openFirewall = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.oci-containers.containers.homebridge = {
+      image = "docker.io/homebridge/homebridge:latest";
+      volumes = [ "${cfg.stateDir}:/homebridge" ];
+      environment = { TZ = config.time.timeZone; };
+      ports = [ "${builtins.toString cfg.port}:${builtins.toString cfg.port}" ];
+      extraOptions = [
+        "--privileged"
+        "--net=host"
+        "--label=io.containers.autoupdate=registry"
+      ];
+    };
+
+    networking.firewall = mkIf cfg.openFirewall {
+      allowedTCPPorts = [ 5353 cfg.port 51241 ];
+      allowedTCPPortRanges = [{
+        from = 52100;
+        to = 52150;
+      }];
+      allowedUDPPorts = [ 5353 cfg.port 51241 ];
+      allowedUDPPortRanges = [{
+        from = 52100;
+        to = 52150;
+      }];
+    };
+  };
+}
