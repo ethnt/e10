99d968270a5e0da2202f83d78e8a6b0ef64879bd
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-07-09 17:26:31 -0400

    Configure Unpoller to send data to Loki (#53)

--- modules/profiles/telemetry/prometheus-unpoller-exporter/satan.nix
+++ modules/profiles/telemetry/prometheus-unpoller-exporter/satan.nix
@@ -6,5 +6,9 @@
     user = "ethnt-unpoller";
     pass = config.sops.secrets.satan_unifi_password.path;
     verify_ssl = false;
+    save_ids = true;
+    save_events = true;
+    save_anomalies = true;
+    save_alarms = true;
   }];
 }
