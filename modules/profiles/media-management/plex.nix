460ae9a58e7bb08567e6b5705e6c084544e8c5f1
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-08-30 10:17:45 -0400

    Create and use local directories for SABnzbd downloads

--- modules/profiles/media-management/plex.nix
+++ modules/profiles/media-management/plex.nix
@@ -1,8 +1,12 @@
-{ lib, ... }: {
+{ config, lib, ... }: {
   services.plex = {
     enable = true;
     openFirewall = true;
   };
+
+  systemd.tmpfiles.rules = [
+    "d '/data/local/tmp/plex/transcode' 0777 ${config.services.plex.user} ${config.services.plex.group} - -"
+  ];
 
   e10.services.backup.jobs.system.exclude =
     lib.mkAfter [ "/var/lib/plex/Plex Media Server/Cache" ];
--- modules/profiles/media-management/sabnzbd.nix
+++ modules/profiles/media-management/sabnzbd.nix
@@ -1,6 +1,11 @@
-{
+{ config, ... }: {
   services.sabnzbd = {
     enable = true;
     openFirewall = true;
   };
+
+  systemd.tmpfiles.rules = [
+    "d '/data/local/tmp/sabnzbd/inter' 0777 ${config.services.sabnzbd.user} ${config.services.sabnzbd.group} - -"
+    "d '/data/local/tmp/sabnzbd/dst' 0777 ${config.services.sabnzbd.user} ${config.services.sabnzbd.group} - -"
+  ];
 }
