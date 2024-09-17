3024e95f611848afe5421e79d0c07239d7d1fc41
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-09-17 08:53:40 -0400

    Remove grant to `grafana` user for Attic's PostgreSQL database

--- modules/profiles/databases/postgresql/atticd.nix
+++ modules/profiles/databases/postgresql/atticd.nix
@@ -10,12 +10,6 @@
       GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO atticd;
       \c atticd postgres
       GRANT ALL ON SCHEMA public TO atticd;
-
-      CREATE ROLE grafana WITH LOGIN PASSWORD 'grafana' CREATEDB;
-      GRANT SELECT ON DATABASE atticd TO grafana;
-      GRANT USAGE ON SCHEMA public TO grafana;
-      GRANT SELECT ON ALL TABLES IN SCHEMA public TO grafana;
-      GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA public TO grafana;
     '';
   };
 }
