4a32126b9f4344275b8a5f441abcf39ae3ed2ad2
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-09-19 00:23:05 -0400

    Update Attic keys (#80)

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "e10.cachix.org-1:/++Tmo/ghEqnLwsQJdXn04c262agRCK5PaPYz8NcVfo="
+      "e10:h64l3MobkrdOGab0Xaambo3ROC/w05Yi70oJ8U8ZEfQ="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
     ];
   };
