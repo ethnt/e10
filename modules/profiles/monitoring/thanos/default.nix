8332b7f2d60703fa06e6ee15f37bf2b5ac164422
Ethan Turkeltaub <ethan@turkeltaub.dev>
2025-10-07 09:53:45 -0400

    Replace Thanos storage configuration secret with SOPS template

--- modules/profiles/monitoring/thanos/default.nix
+++ modules/profiles/monitoring/thanos/default.nix
@@ -1,10 +1,49 @@
 { config, ... }: {
-  sops.secrets = {
-    thanos_sidecar_object_storage_configuration = {
-      format = "yaml";
-      sopsFile = ./secrets.yml;
+  sops = {
+    secrets = {
+      thanos_sidecar_object_storage_configuration = {
+        format = "yaml";
+        sopsFile = ./secrets.yml;
+        mode = "0777";
+        owner = "prometheus";
+      };
+
+      thanos_storage_bucket = {
+        format = "yaml";
+        sopsFile = ./secrets.yml;
+      };
+
+      thanos_storage_aws_access_key_id = {
+        format = "yaml";
+        sopsFile = ./secrets.yml;
+      };
+
+      thanos_storage_aws_secret_access_key = {
+        format = "yaml";
+        sopsFile = ./secrets.yml;
+      };
+    };
+
+    templates.thanos_sidecar_object_storage_configuration = {
+      content = builtins.toJSON ({
+        type = "S3";
+        config = {
+          bucket = config.sops.placeholder.thanos_storage_bucket;
+          endpoint = "s3.us-east-2.amazonaws.com";
+          region = "us-east-2";
+          access_key = config.sops.placeholder.thanos_storage_aws_access_key_id;
+          secret_key =
+            config.sops.placeholder.thanos_storage_aws_secret_access_key;
+        };
+      });
       mode = "0777";
       owner = "prometheus";
+      restartUnits = [
+        "thanos-sidecar.service"
+        "thanos-store.service"
+        "thanos-query.service"
+        "thanos-compact.service"
+      ];
     };
   };
 
@@ -14,7 +53,7 @@
       http.port = 19190;
       grpc.port = 10900;
       objstore.config-file =
-        config.sops.secrets.thanos_sidecar_object_storage_configuration.path;
+        config.sops.templates.thanos_sidecar_object_storage_configuration.path;
     };
 
     store = {
@@ -22,7 +61,7 @@
       http.port = 19191;
       grpc.port = 10901;
       objstore.config-file =
-        config.sops.secrets.thanos_sidecar_object_storage_configuration.path;
+        config.sops.templates.thanos_sidecar_object_storage_configuration.path;
     };
 
     query = {
@@ -39,7 +78,19 @@
       enable = true;
       http.port = 19193;
       objstore.config-file =
-        config.sops.secrets.thanos_sidecar_object_storage_configuration.path;
+        config.sops.templates.thanos_sidecar_object_storage_configuration.path;
     };
+  };
+
+  systemd.services = let
+    additionalServiceConfig = {
+      wants = [ "sops-nix.service" ];
+      after = [ "sops-nix.service" ];
+    };
+  in {
+    thanos-sidecar = additionalServiceConfig;
+    thanos-store = additionalServiceConfig;
+    thanos-query = additionalServiceConfig;
+    thanos-compact = additionalServiceConfig;
   };
 }
