566f35bfa3bfa4497b41358181d6bcb0874a4320
Ethan Turkeltaub <ethan@turkeltaub.dev>
2025-10-25 00:39:54 -0400

    [build] Update `postfix` options

--- modules/profiles/communications/postfix/default.nix
+++ modules/profiles/communications/postfix/default.nix
@@ -7,14 +7,15 @@
 
   services.postfix = {
     enable = true;
-    relayHost = "email-smtp.us-east-2.amazonaws.com";
-    relayPort = 587;
-    config = {
-      smtp_use_tls = "yes";
-      smtp_sas_auth_enable = "yes";
-      smtp_sasl_security_options = "";
-      smtp_sasl_password_maps =
-        "texthash:${config.sops.secrets.postfix_sasl_password.path}";
+    settings = {
+      main = {
+        relayhost = [ "email-smtp.us-east-2.amazonaws.com:587" ];
+        smtp_use_tls = "yes";
+        smtp_sas_auth_enable = "yes";
+        smtp_sasl_security_options = "";
+        smtp_sasl_password_maps =
+          "texthash:${config.sops.secrets.postfix_sasl_password.path}";
+      };
     };
   };
 }
