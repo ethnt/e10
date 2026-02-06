15b8f0492a5c5873e825be29bf1176241b519cad
Ethan Turkeltaub <ethan@turkeltaub.dev>
2026-02-06 08:17:37 -0500

    Fix stash issue

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -2,6 +2,7 @@ _: {
   perSystem = { pkgs, ... }: {
     packages = {
       bentopdf = pkgs.callPackage ./bentopdf { };
+      eufy-security-ws = pkgs.callPackage ./eufy-security-ws { };
       fileflows = pkgs.callPackage ./fileflows { };
       mongodb-ce-6_0 = pkgs.callPackage ./mongodb-ce-6_0 { };
       tracearr = pkgs.callPackage ./tracearr { };
