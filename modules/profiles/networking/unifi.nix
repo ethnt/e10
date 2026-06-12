bc2ce12e7a21137c979758bea204d20c63a807a6
Ethan Turkeltaub <ethan@turkeltaub.dev>
2026-06-11 22:19:28 -0400

    Clean up

--- modules/profiles/networking/unifi.nix
+++ modules/profiles/networking/unifi.nix
@@ -6,9 +6,4 @@
     openFirewallUiPort = true;
     openFirewallServicePorts = true;
   };
-
-  # networking.firewall = {
-  #   allowedTCPPorts = [ 6789 8080 8880 8443 8843 ];
-  #   allowedUDPPorts = [ 8443 ];
-  # };
 }
