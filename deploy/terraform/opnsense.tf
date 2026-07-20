370f1b739187b8e4d171892bc2a3ccba17a6dcc3
Ethan Turkeltaub <ethan@turkeltaub.dev>
2026-07-19 23:58:34 -0400

    Stricter IoT firewall rules, update `opnsense` Terraform provider

--- deploy/terraform/opnsense.tf
+++ deploy/terraform/opnsense.tf
@@ -1,10 +1,13 @@
 locals {
-  interfaces = {
+  trusted_interfaces = {
     LAN        = "lan"
     Management = "opt1"
-    IoT        = "opt5"
     Tailscale  = "opt3"
     Homelab    = "opt2"
+  }
+
+  untrusted_interfaces = {
+    IoT = "opt5"
   }
 
   vlans = {
@@ -33,7 +36,7 @@ locals {
 resource "opnsense_firewall_alias" "private_networks" {
   name        = "PrivateNetworks"
   type        = "network"
-  content     = ["10.0.0.0/8", "192.168.1.0/16", "172.16.0.0/12", "192.168.0.0/16"]
+  content     = ["10.2.0.0/16", "10.10.0.0/16", "10.50.0.0/16", "192.168.1.0/16", "172.16.0.0/12", "192.168.0.0/16"]
   description = "[Terraform] All RFC 1918 private network addresses"
 }
 
@@ -72,56 +75,177 @@ resource "opnsense_firewall_alias" "denylist_geoip" {
   description = "[Terraform] Countries to deny access to"
 }
 
-resource "opnsense_firewall_filter" "interface_to_any" {
-  for_each = local.interfaces
+resource "opnsense_firewall_filter" "trusted_interface_to_any" {
+  for_each = local.trusted_interfaces
 
   enabled = true
 
-  direction = "in"
-  action    = "pass"
-  interface = [each.value]
-  source = {
-    net = each.value
+  interface = {
+    interface = [each.value]
   }
-  destination = {
-    net = "any"
+
+  filter = {
+    quick     = true
+    direction = "in"
+    action    = "pass"
+    protocol  = "any"
+
+    source = {
+      net = each.value
+    }
+
+    destination = {
+      net = "any"
+    }
+
+    log = true
   }
-  protocol    = "any"
+
   description = "[Terraform] ${each.key} to any rule"
-  log         = true
 }
 
-resource "opnsense_firewall_filter" "interface_ipv6_to_any" {
-  for_each = local.interfaces
+resource "opnsense_firewall_filter" "trusted_interface_ipv6_to_any" {
+  for_each = local.trusted_interfaces
 
   enabled = true
 
-  direction = "in"
-  action    = "pass"
-  interface = [each.value]
-  source = {
-    net = each.value
+  interface = {
+    interface = [each.value]
   }
-  destination = {
-    net = "any"
+
+  filter = {
+    quick       = true
+    direction   = "in"
+    action      = "pass"
+    protocol    = "any"
+    ip_protocol = "inet6"
+
+    source = {
+      net = each.value
+    }
+
+    destination = {
+      net = "any"
+    }
+
+    log = true
   }
-  protocol    = "any"
-  ip_protocol = "inet6"
+
   description = "[Terraform] ${each.key} to any rule"
-  log         = true
 }
 
 resource "opnsense_firewall_filter" "denylist_geoip_to_wan" {
-  enabled   = true
-  direction = "in"
-  action    = "reject"
-  interface = ["wan"]
-  source = {
-    net = "Denylist_GeoIP"
+  enabled = true
+
+  interface = {
+    interface = ["wan"]
   }
-  protocol    = "any"
+
+  filter = {
+    quick     = true
+    direction = "in"
+    action    = "reject"
+    protocol  = "any"
+
+    source = {
+      net = "Denylist_GeoIP"
+    }
+
+    log = true
+  }
+
   description = "[Terraform] Denylist GeoIP to WAN rule"
-  log         = true
+}
+
+resource "opnsense_firewall_filter" "untrusted_interface_to_dns" {
+  for_each = local.untrusted_interfaces
+
+  enabled  = true
+  sequence = 1
+
+  interface = {
+    interface = [each.value]
+  }
+
+  filter = {
+    quick     = true
+    direction = "in"
+    action    = "pass"
+    protocol  = "TCP/UDP"
+
+    source = {
+      net = each.value
+    }
+
+    destination = {
+      net  = "${each.value}ip"
+      port = "53"
+    }
+
+    log = true
+  }
+
+  description = "[Terraform] ${each.key} to DNS rule"
+}
+
+resource "opnsense_firewall_filter" "untrusted_interface_to_private_networks" {
+  for_each = local.untrusted_interfaces
+
+  enabled  = true
+  sequence = 2
+
+  interface = {
+    interface = [each.value]
+  }
+
+  filter = {
+    quick     = true
+    direction = "in"
+    action    = "block"
+    protocol  = "any"
+
+    source = {
+      net = each.value
+    }
+
+    destination = {
+      net = "PrivateNetworks"
+    }
+
+    log = true
+  }
+
+  description = "[Terraform] ${each.key} to private networks block rule"
+}
+
+resource "opnsense_firewall_filter" "untrusted_interface_to_any" {
+  for_each = local.untrusted_interfaces
+
+  enabled  = true
+  sequence = 3
+
+  interface = {
+    interface = [each.value]
+  }
+
+  filter = {
+    quick     = true
+    direction = "in"
+    action    = "pass"
+    protocol  = "any"
+
+    source = {
+      net = each.value
+    }
+
+    destination = {
+      net = "any"
+    }
+
+    log = true
+  }
+
+  description = "[Terraform] ${each.key} to any (internet) rule"
 }
 
 resource "opnsense_interfaces_vlan" "vlan" {
