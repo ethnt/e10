f8e590bb04f44aa4946d6ed8435a0043b41658fa
Ethan Turkeltaub <ethan@turkeltaub.dev>
2026-09-10 22:03:13 -0400

    Fix `postfix` implementation

--- modules/profiles/communications/postfix/default.nix
+++ modules/profiles/communications/postfix/default.nix
@@ -1,20 +1,37 @@
 { config, ... }: {
-  sops.secrets.postfix_sasl_password = {
-    owner = config.services.postfix.user;
-    sopsFile = ./secrets.yml;
-    format = "yaml";
+  sops = {
+    secrets = {
+      postfix_smtp2go_username = {
+        owner = config.services.postfix.user;
+        sopsFile = ./secrets.yml;
+        format = "yaml";
+      };
+      postfix_smtp2go_password = {
+        owner = config.services.postfix.user;
+        sopsFile = ./secrets.yml;
+        format = "yaml";
+      };
+    };
+
+    templates.postfix_sasl_password_maps = {
+      content = ''
+        [mail.smtp2go.com]:2525 ${config.sops.placeholder.postfix_smtp2go_username}:${config.sops.placeholder.postfix_smtp2go_password}
+      '';
+      owner = config.services.postfix.user;
+    };
   };
 
   services.postfix = {
     enable = true;
     settings = {
       main = {
-        relayhost = [ "email-smtp.us-east-2.amazonaws.com:587" ];
-        smtp_use_tls = "yes";
-        smtp_sas_auth_enable = "yes";
-        smtp_sasl_security_options = "";
-        smtp_sasl_password_maps = "texthash:${config.sops.secrets.postfix_sasl_password.path}";
+        relayhost = [ "[mail.smtp2go.com]:2525" ];
+        smtp_tls_security_level = "encrypt";
+        smtp_sasl_auth_enable = "yes";
+        smtp_sasl_security_options = "noanonymous";
+        smtp_sasl_password_maps = "texthash:${config.sops.templates.postfix_sasl_password_maps.path}";
       };
     };
+    setSendmail = true;
   };
 }
